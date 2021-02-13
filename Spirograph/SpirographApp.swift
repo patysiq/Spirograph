32da8c03aef6f8ec280bc7ef188f66ac46bd0bcb
Patricia <patysiq@yahoo.com.br>
2021-02-13 10:57:29 -0300

    Initial Commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by PATRICIA S SIQUEIRA on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
